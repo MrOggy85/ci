d866d23763bf8829d127bd114fcf2e0fc4f9ca01
Oskar Lindgren <lindgren.oskar@gmail.com>
2019-02-11 18:35:38 +0900

    add all parts to japanese frontent build script

--- src/scripts/speaking-japanese-frontend.sh
+++ src/scripts/speaking-japanese-frontend.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-echo "0 $0"
-echo "1 $1"
-echo "2 $2"
+pwd
+
+echo 'Removing folder "speaking-japanese"'
+rm -rf speaking-japanese 
+
+echo 'git clone speaking-japanese'
+git clone git@github.com:MrOggy85/speaking-japanese.git
+
+cd speaking-japanese
 
 pwd
 
-git clone git@github.com:MrOggy85/speaking-japanese-api.git
+echo 'npm install & build'  
+npm install && npm run build
 
+echo 'replace current files with newly built'
+#rm -rf -v /usr/share/nginx/html/speaking-japanese && \
+#mkdir /usr/share/nginx/html/speaking-japanese && \
+#mv -v ./dist/* /usr/share/nginx/html/speaking-japanese/
 
-
-#git status
+echo 'DONE'
 
 exit 0
