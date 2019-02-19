a882eafd86a2f938d2730edc7dc70980fe499a0d
Oskar Lindgren <lindgren.oskar@gmail.com>
2019-02-20 01:17:45 +0900

    add set -e to script to exit if any command fails

--- src/scripts/speaking-japanese-frontend.sh
+++ src/scripts/speaking-japanese-frontend.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+set -e
+
 pwd
 
 whoami
 
 echo 'Removing folder "speaking-japanese"'
-rm -rf speaking-japanese 
+rm -rf speaking-japanese
 
 echo 'git clone speaking-japanese'
 git clone https://github.com/MrOggy85/speaking-japanese.git
@@ -14,12 +16,19 @@ cd speaking-japanese
 
 pwd
 
-echo 'npm install & build'  
-npm install && npm run build
+echo 'npm install'
+npm install
 
-echo 'replace current files with newly built'
-rm -rf -v /www/speaking-japanese && \
-mkdir /www/speaking-japanese && \
+echo 'npm run build'
+npm run build
+
+echo 'rm -rf -v /www/speaking-japanese'
+rm -rf -v /www/speaking-japanese
+
+echo 'mkdir /www/speaking-japanese'
+mkdir /www/speaking-japanese
+
+echo 'mv -v ./dist/* /www/speaking-japanese/'
 mv -v ./dist/* /www/speaking-japanese/
 
 echo 'DONE'
